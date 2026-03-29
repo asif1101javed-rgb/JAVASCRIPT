d4a5f525f7272b885c219ccf9bd821a6f6653642
Asif-Ali-1101 <asif.1101.javed@gmail.com>
2026-03-29 20:20:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// alert("Hello Asif Ali")
+
+// alert("HELLO WORLD")
+
+
+// var a=85544586687;
+// alert(a)
+
+// alert("number if a is +a")
+
+var UserName = "Asif"
+var UserContact = "123456789"
+
+alert("User Name is : " + UserName)
+alert("Variable type is : " + typeof UserName)
+alert("Asif Contact Number is :" +UserContact)
