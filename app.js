3a5c43d983f9dfa6bb06ec513006bf196594658d
Asif-Ali-1101 <asif.1101.javed@gmail.com>
2026-04-05 20:07:30 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-// alert("Hello Asif Ali")
+// alert("hello world")
+// alert("JavaScript")
+// alert('Hello')
+// alert('Asif Ali')
 
-// alert("HELLO WORLD")
+// es5
+// var friendName = "Aqib";
+// var friendContact = 38375575;
+// var friendAge = 12;
+// var closeFriend = true
+
+// alert("Friend Name variable Type of : "+ typeof(friendName))
+// alert("closeFriend variable Type of : "+ typeof(closeFriend))
+
+// alert("1) Friend Name is " + friendName)
+// alert("2) Friend Contact is " + friendContact)
+// alert("3) Friend Age is " + friendAge)
+
+// In List form
+
+// alert("1) Friend Name is : " + friendName + "\n2) Friend Contact is : " + friendContact + "\n3) Friend Age is : " + friendAge)
 
 
-// var a=85544586687;
-// alert(a)
 
-// alert("number if a is +a")
+// var userInput = prompt("Enter User Name" , "Asif Ali")
+// alert("User Name is " + userInput)
 
-var UserName = "Asif"
-var UserContact = "123456789"
 
-alert("User Name is : " + UserName)
-alert("Variable type is : " + typeof UserName)
-alert("Asif Contact Number is :" +UserContact)
+var number1 = +prompt("Number of A is :")
+var number2 = +prompt("Number of B is :")
+alert("Answer of " +number1 + " + " + number2 +" = " + (number1+number2))
+alert("Answer of " +number1 + " - " + number2 +" = " + (number1-number2))
+alert("Answer of " +number1 + " / " + number2 +" = " + (number1/number2))
+alert("Answer of " +number1 + " * " + number2 +" = " + (number1*number2))
